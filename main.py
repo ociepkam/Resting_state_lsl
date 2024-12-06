e71ed529aab89a8fc4e0baf87faa6d5afd3c080b
ociepkam <ociepka.m@gmail.com>
2024-12-06 11:06:48 +0100

    v1.0 added lsl triggers

--- main.py
+++ main.py
@@ -19,16 +19,35 @@ def main():
     fixation = visual.TextStim(win, color=config["fixation_color"], text=config["fixation_text"],
                                height=config["fixation_size"])
 
+    if config["send_lsl_triggers"]:
+        from pylsl import StreamInfo, StreamOutlet
+        lsl_info = StreamInfo(name="TriggerStream", type="Triggers", channel_count=1, nominal_srate=0, channel_format="int32", source_id="myuid34234")
+        lsl_outlet = StreamOutlet(info=lsl_info)
+    else:
+        lsl_outlet = None
+
     # --------------------------------------- procedure ---------------------------------------
+    # instruction
     show_info(win=win, file_name=join("messages", "start.txt"), text_size=config["text_size"], text_color=config["text_color"], screen_res=screen_res)
+
+    # preparation
     fixation.setAutoDraw(True)
     win.callOnFlip(clock.reset)
+    if config["send_lsl_triggers"]:
+        win.callOnFlip(lsl_outlet.push_sample(x=[1]))
     win.flip()
 
+    # mian loop
     while clock.getTime() < config["rest_time"]:
         check_exit(key=config["exit_key"])
 
+    # cleaning
     fixation.setAutoDraw(False)
+    if config["send_lsl_triggers"]:
+        win.callOnFlip(lsl_outlet.push_sample(x=[1]))
+    win.flip()
+
+    # end screen
     show_info(win=win, file_name=join("messages", "end.txt"), text_size=config["text_size"], text_color=config["text_color"], screen_res=screen_res)
 
 
